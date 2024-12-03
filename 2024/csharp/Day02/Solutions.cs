7ecc7aeab738db5e2fa3993518327a2f0c39c7c7
Steffan Tucker <40306798+steffantucker@users.noreply.github.com>
2024-12-03 00:42:33 -0800

    [day1]finish

--- 2024/csharp/Day02/Solutions.cs
+++ 2024/csharp/Day02/Solutions.cs
@@ -5,12 +5,17 @@ public class Solutions
     static void Main(string[] args)
     {
         int[][] input = Utils.Input.GetMatrixInput("02");
-        int part1 = input.Select(IsExcessivelySafe).Count(b => b);
+        int part1 = input.Where(IsSafe).Count();
         Console.WriteLine($"Part 1: {part1}");
+        int part2 = input.Where(IsPermissivelySafe).Count();
+        Console.WriteLine($"Part 2: {part2}");
     }
 
-    public static bool IsExcessivelySafe(int[] report)
+    public static bool IsSafe(int[] report)
     {
+        // report.Zip(report.Skip(1), (a, b) => a - b);
+        // then all(x => x >= -3 && x <= -1) || all(x => x >= 1 && x <= 3)
+        // ^ entirety of safe check, if run on diffs
         bool isDecreasing = (report[0] > report[1]);
         for (int i = 0; i < report.Length - 1; i++)
         {
@@ -29,7 +34,7 @@ public class Solutions
 
     public static bool IsPermissivelySafe(int[] report)
     {
-        if (IsExcessivelySafe(report)) return true;
-        return true;
+        return IsSafe(report) || Enumerable.Range(0, report.Count())
+            .Any(i => IsSafe(report.Take(i).Concat(report.Skip(i+1)).ToArray()));
     }
 }
--- 2024/csharp/DaysTests/Day02Tests.cs
+++ 2024/csharp/DaysTests/Day02Tests.cs
@@ -1,4 +1,5 @@
 ﻿using Day02;
+
 namespace DaysTests;
 
 public class Day02Tests
@@ -10,17 +11,38 @@ public class Day02Tests
     [TestCase(new[] { 1, 1, 2 })]
     [TestCase(new[] { 2, 6, 1 })]
     [TestCase(new[] { 1, 2, 5, 9 })]
-    public void IsExcessivelySafe_ReturnsFalse(int[] input)
+    public void IsSafe_ReturnsFalse(int[] input)
     {
-        bool result = Solutions.IsExcessivelySafe(input);
+        bool result = Solutions.IsSafe(input);
         Assert.That(result, Is.False, $"{input} should not have been safe");
     }
-    
-    [TestCase(new[] {7, 6, 4, 2, 1})]
-    [TestCase(new[] {1, 3, 6, 7, 9})]
-    public void IsExcessivelySafe_ReturnsTrue(int[] input)
+
+    [TestCase(new[] { 7, 6, 4, 2, 1 })]
+    [TestCase(new[] { 1, 3, 6, 7, 9 })]
+    public void IsSafe_ReturnsTrue(int[] input)
     {
-        bool result = Solutions.IsExcessivelySafe(input);
+        bool result = Solutions.IsSafe(input);
+        Assert.That(result, Is.True, $"{input} should have been safe");
+    }
+
+    [TestCase(new[] { 1, 2, 7, 8, 9 })]
+    [TestCase(new[] { 9, 7, 6, 2, 1 })]
+    public void IsPermissivelySafe_ReturnsFalse(int[] input)
+    {
+        bool result = Solutions.IsPermissivelySafe(input);
+        Assert.That(result, Is.False, $"{input} should not have been safe");
+    }
+
+    [TestCase(new[] { 7, 6, 4, 2, 1 })]
+    [TestCase(new[] { 1, 3, 2, 4, 5 })]
+    [TestCase(new[] { 8, 6, 4, 4, 1 })]
+    [TestCase(new[] { 1, 3, 6, 7, 9 })]
+    [TestCase(new[] { 1, 1, 2 })]
+    [TestCase(new[] { 2, 6, 1 })]
+    [TestCase(new[] { 1, 2, 5, 9 })]
+    public void IsPermissivelySafe_ReturnsTrue(int[] input)
+    {
+        bool result = Solutions.IsPermissivelySafe(input);
         Assert.That(result, Is.True, $"{input} should have been safe");
     }
 }

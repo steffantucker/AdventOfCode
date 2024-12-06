104dd9389ed1fbf591ca19b8e146f19904e59617
Steffan Tucker <40306798+steffantucker@users.noreply.github.com>
2024-12-05 17:23:27 -0800

    [fix] consistent variable names in tests Updated variable names in tests to be consistent now that i'm not sleepy while writing the test

--- 2024/csharp/DaysTests/Day02Tests.cs
+++ 2024/csharp/DaysTests/Day02Tests.cs
@@ -13,24 +13,24 @@ public class Day02Tests
     [TestCase(new[] { 1, 2, 5, 9 })]
     public void IsSafe_ReturnsFalse(int[] input)
     {
-        bool result = Solutions.IsSafe(input);
-        Assert.That(result, Is.False, $"{input} should not have been safe");
+        bool actual = Day02.Day02.IsSafe(input);
+        Assert.That(actual, Is.False, $"{input} should not have been safe");
     }
 
     [TestCase(new[] { 7, 6, 4, 2, 1 })]
     [TestCase(new[] { 1, 3, 6, 7, 9 })]
     public void IsSafe_ReturnsTrue(int[] input)
     {
-        bool result = Solutions.IsSafe(input);
-        Assert.That(result, Is.True, $"{input} should have been safe");
+        bool actual = Day02.Day02.IsSafe(input);
+        Assert.That(actual, Is.True, $"{input} should have been safe");
     }
 
     [TestCase(new[] { 1, 2, 7, 8, 9 })]
     [TestCase(new[] { 9, 7, 6, 2, 1 })]
     public void IsPermissivelySafe_ReturnsFalse(int[] input)
     {
-        bool result = Solutions.IsPermissivelySafe(input);
-        Assert.That(result, Is.False, $"{input} should not have been safe");
+        bool actual = Day02.Day02.IsPermissivelySafe(input);
+        Assert.That(actual, Is.False, $"{input} should not have been safe");
     }
 
     [TestCase(new[] { 7, 6, 4, 2, 1 })]
@@ -42,7 +42,7 @@ public class Day02Tests
     [TestCase(new[] { 1, 2, 5, 9 })]
     public void IsPermissivelySafe_ReturnsTrue(int[] input)
     {
-        bool result = Solutions.IsPermissivelySafe(input);
-        Assert.That(result, Is.True, $"{input} should have been safe");
+        bool actual = Day02.Day02.IsPermissivelySafe(input);
+        Assert.That(actual, Is.True, $"{input} should have been safe");
     }
 }
